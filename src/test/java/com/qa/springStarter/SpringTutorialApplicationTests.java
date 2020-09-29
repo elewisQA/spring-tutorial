effef0a284f005f737b548845798b9f225e4fc92
Elizabeth Lewis <elewis@academytrainee.com>
2020-09-29 10:22:39 +0100

    Initial files

---
+++ src/test/java/com/qa/springStarter/SpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.springStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
